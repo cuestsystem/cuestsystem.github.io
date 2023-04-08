1a9cee1919bb60958f2fc073ac715f23192171cd
cuestsystem <cuest.system@gmail.com>
2023-04-08 08:30:13 -0600

    cambio

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -47,7 +47,7 @@ class MiNav extends HTMLElement {
         html += /* html */
           `<li>
             <a href=
-"alumnos.html">Galaxia</a>
+"alumnos.html">Galaxias</a>
           </li>`;
       }
       this.ul.innerHTML += html;
